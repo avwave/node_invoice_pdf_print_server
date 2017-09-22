7a40d6ab931f047d5508b333df85acf5e563aa0f
Ray Arvin Rimorin <rayarvin@gmail.com>
2017-09-23 06:18:20 +0800

    increase arg limit

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var express = require('express'),
   puppeteer = require('puppeteer'),
   render = require('./render')
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
 
 var port = process.env.PORT || 8080;
 
